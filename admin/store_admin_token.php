c2c4d847b31aab28051310cbc75d2092bfd6a60c
tanvirarifsiddiqui <tanvirtmm@gmail.com>
2023-11-09 08:24:33 +0600

    solved multiple token storing problem

--- admin/store_admin_token.php
+++ admin/store_admin_token.php
@@ -5,14 +5,22 @@ include '../connection.php';
 $adminId = $_POST['admin_id'];    
 $token = $_POST['token'];    
 
-// Insert the data into the database.
-$sqlQuery = "INSERT INTO admin_tokens (admin_id, token) VALUES ($adminId, '$token')";
+// Check if the token already exists in the database.
+$checkQuery = "SELECT token FROM admin_tokens WHERE admin_id = $adminId AND token = '$token'";
+$checkResult = $connectNow->query($checkQuery);
 
-$resultOfQuery = $connectNow->query($sqlQuery);
+if ($checkResult->num_rows == 0) {
+    // Token does not exist, so we can insert it.
+    $insertQuery = "INSERT INTO admin_tokens (admin_id, token) VALUES ($adminId, '$token')";
+    $insertResult = $connectNow->query($insertQuery);
 
-if ($resultOfQuery) {
-    echo json_encode(array("success" => true));
-} else {
-    echo json_encode(array("success" => false));
+    if ($insertResult) {
+        echo json_encode(array("success" => true, "message" => "Successfully stored token"));
+    } else {
+        echo json_encode(array("success" => false));
+    }
+}else{
+    // Token already exists, do not insert.
+    echo json_encode(array("success" => true, "message" => "Token already exists"));
 }
 ?>
--- user/store_user_token.php
+++ user/store_user_token.php
@@ -1,18 +1,25 @@
 <?php
 include '../connection.php';
 
-
 $userId = $_POST['user_id'];    
 $token = $_POST['token'];    
 
-// Insert the data into the database.
-$sqlQuery = "INSERT INTO user_tokens (user_id, token) VALUES ($userId, '$token')";
+// Check if the token already exists in the database.
+$checkQuery = "SELECT token FROM user_tokens WHERE user_id = $userId AND token = '$token'";
+$checkResult = $connectNow->query($checkQuery);
 
-$resultOfQuery = $connectNow->query($sqlQuery);
+if ($checkResult->num_rows == 0) {
+    // Token does not exist, so we can insert it.
+    $insertQuery = "INSERT INTO user_tokens (user_id, token) VALUES ($userId, '$token')";
+    $insertResult = $connectNow->query($insertQuery);
 
-if ($resultOfQuery) {
-    echo json_encode(array("success" => true));
+    if ($insertResult) {
+        echo json_encode(array("success" => true, "message" => "Successfully stored token"));
+    } else {
+        echo json_encode(array("success" => false));
+    }
 } else {
-    echo json_encode(array("success" => false));
+    // Token already exists, do not insert.
+    echo json_encode(array("success" => true, "message" => "Token already exists"));
 }
 ?>
